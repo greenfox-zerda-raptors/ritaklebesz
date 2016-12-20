b3261279e702e457dd415f74f029b2da18cec3f6
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-20 21:05:56 +0100

    W9D2 Todo Web App: listing extended

--- week-09/day-2/todoApp/src/main/java/com/greenfoxacademy/TodoController.java
+++ week-09/day-2/todoApp/src/main/java/com/greenfoxacademy/TodoController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Rita on 2016-12-20.
@@ -20,8 +24,19 @@ public class TodoController {
     }
 
     @RequestMapping(value = {"/", "/list"})
-    public String list(Model model) {
-        model.addAttribute("todos", todoService.getTodos());
+    public String list(Model model, @RequestParam(name = "active", required = false) String active) {
+        List<Todo> list = todoService.getTodos();
+        List<Todo> result = new ArrayList<>();
+        if (active != null && active.equals("active")) {
+            for (Todo todo : list) {
+                if (!todo.isDone()) {
+                    result.add(todo);
+                }
+            }
+        } else {
+            result = list;
+        }
+        model.addAttribute("todos", result);
         return "todo";
     }
 }
