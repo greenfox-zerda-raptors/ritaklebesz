76f9b357646d6010a5ef891f2edf5172b1da355a
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-23 23:23:00 +0100

    W9 Project initial commit

---
+++ week-09/Project/calorieCounter/src/main/java/com/greenfoxacademy/calorieCounter/controller/CounterController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.calorieCounter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rita on 2016-12-23.
+ */
+
+@Controller
+@RequestMapping("/caloriecounter")
+public class CounterController {
+
+    @RequestMapping(value = {"/index", "/"})
+    public String index() {
+        return "index";
+    }
+}
