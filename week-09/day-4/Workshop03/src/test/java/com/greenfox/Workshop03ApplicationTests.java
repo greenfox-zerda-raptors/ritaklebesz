08fefc30ce5b8bfbf6a4cdd1ce9ac4f8acd610c2
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-29 17:16:03 +0100

    W9D4 WS3: add first test

--- week-09/day-4/Workshop03/src/test/java/com/greenfox/Workshop03ApplicationTests.java
+++ week-09/day-4/Workshop03/src/test/java/com/greenfox/Workshop03ApplicationTests.java
@@ -1,17 +1,31 @@
 package com.greenfox;
 
+import com.greenfox.repository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Workshop03Application.class)
+@WebAppConfiguration
 public class Workshop03ApplicationTests {
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Test
-    public void contextLoads() {
+    @Sql({"/clear-tables.sql", "/populate-tables.sql"})
+    @Transactional
+    public void shouldReturnManyPhonesForCustomer() {
+        Customer myself = customerRepository.findByName("Marcio");
+        assertThat(myself.getPhoneNumbers().size(), is(2));
     }
-
 }
