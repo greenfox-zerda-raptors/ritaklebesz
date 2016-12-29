6876fd9d53e0b5d88b646ea4c2e57fb95c4fca42
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-29 15:49:14 +0100

    W9D4 WS3: Item done

---
+++ week-09/day-4/Workshop03/src/main/java/com/greenfox/Item.java
@@ -0,0 +1,23 @@
+package com.greenfox;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
+@Setter
+@Getter
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
