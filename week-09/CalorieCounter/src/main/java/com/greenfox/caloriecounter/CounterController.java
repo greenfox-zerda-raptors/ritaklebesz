ca468a7691d7b3efd44156ac824e6386851ccec9
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-30 10:30:17 +0100

    Calorie counter project restarted

---
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/CounterController.java
@@ -0,0 +1,23 @@
+package com.greenfox.caloriecounter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rita on 2016-12-30.
+ */
+@Controller
+@RequestMapping("/caloriecounter")
+public class CounterController {
+
+    @RequestMapping(value = {"/index", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = {"/add"})
+    public String addOrEdit() {
+        return "add";
+    }
+}
+
