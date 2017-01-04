852dad94fc6b75853b4bd916e529c5089913f200
Rita Klebesz <ritaklebesz@gmail.com>
2017-01-04 15:03:20 +0100

    Reddit Project: new post can be added

--- week-10/Reddit/src/main/java/com/greenfox/rita/reddit/controller/PostController.java
+++ week-10/Reddit/src/main/java/com/greenfox/rita/reddit/controller/PostController.java
@@ -1,9 +1,13 @@
 package com.greenfox.rita.reddit.controller;
 
+import com.greenfox.rita.reddit.Post;
 import com.greenfox.rita.reddit.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,8 +29,16 @@ public class PostController {
         return "index";
     }
 
-    @RequestMapping(value = "/add")
-    public String add() {
+    @GetMapping(value = "/add")
+    public String add(Model model) {
+        model.addAttribute("post", new Post());
         return "add";
+    }
+
+    @PostMapping(value = "/add")
+    public String submitNewPost(@ModelAttribute Post post) {
+        post.setScore(0);
+        repository.save(post);
+        return "redirect:/";
     }
 }
